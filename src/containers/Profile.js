794bdd4a743049e38320a187ac8c4f4d69657480
CraZyNut <nutkungza05@hotmail.com>
2018-11-07 13:47:12 +0700

    Update src/containers/Profile.js

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -5,21 +5,43 @@ import { Button, Avatar } from 'react-native-elements';
 
 
 class Profile extends Component{
-
-
-
-
-
+    
+    
 
     render(){
+        const styles = StyleSheet.create({
+            container: {
+              borderRadius: 4,
+              borderWidth: 0.5,
+              borderColor: '#d6d7da',
+              alignItems: 'center'
+            },
+            title: {
+              fontSize: 30,
+              fontWeight: 'bold',
+              textAlign: 'center',
+            },
+            activeTitle: {
+              color: 'red',
+            },
+          });
         const {authReducer} = this.props
-        return <View>
-            <Text>Your Profile</Text>
+        return <View style={styles.container}>
+            <Text style={[styles.title, this.props.isActive && styles.activeTitle]}>Your Profile</Text>
             {authReducer.isAuth && <View>
-                <Avatar xlarge rounded source={{ uri: authReducer.data.photoURL }} onPress={() => console.log("Works!")} />
+                <Avatar style = {styles.litle} xlarge rounded source={{ uri: authReducer.data.photoURL }} onPress={() => console.log("Works!")} />
                 <Text>{authReducer.data.displayName}</Text>
                 <Text>{authReducer.data.Email}</Text>
 {/* //TODO แสดง เลเวล ค่าประสบการณ์ Objectที่เก็บข้อมูล = authReducer.data.levelQ */}
+                 <Text>Your Score</Text>
+                     {
+                         this.current!=null &&<View>
+                          <Text>Rank: {this.current.index+1} : {this.current.data.name}</Text>
+                        <Text>     Star:  {this.current.data.score}</Text>
+                         </View>
+                      }
+                <Text style = {styles.litle} >Level:</Text>
+                <Text style = {styles.litle} >Experience</Text>
               </View>}
           </View>;
     }
