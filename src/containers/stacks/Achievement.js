e3593ce224dbd2006f0f8ea7db7902986dce4df9
0Empty0 <59010679@kmitl.ac.th>
2018-11-15 01:16:59 +0700

    นั่นแหละ

--- src/containers/stacks/Achievement.js
+++ src/containers/stacks/Achievement.js
@@ -1,12 +1,21 @@
-import { View,Text,StyleSheet } from 'react-native';
+import { View,Text,StyleSheet,Image } from 'react-native';
 import React,{Component} from 'react';
 import { connect } from "react-redux";
 import { Button, Avatar } from 'react-native-elements';
-
-
+import firebase from '../../config/firebase';
 class Achievement extends Component{
-    
-         componentDidUpdate(prevProps, prevState, snapshot) {
+constructor(props){
+    super(props);
+    this.state={
+        achieve:{}
+    }
+}
+    componentDidMount(){
+        this.setState({
+            ...this.props.authReducer.data
+        })
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
              if (prevProps.authReducer.data != this.props.authReducer.data) {
                  this.setState({
                      ...this.props.authReducer.data
@@ -14,40 +23,55 @@ class Achievement extends Component{
              }
          }
 
+     getUrlImage=(path)=>{
+        var storage = firebase.storage();
+        var Ref = storage.child(path).getDownloadURL().then(function (url) {
+            // `url` is the download URL for 'images/stars.jpg'
+
+            // This can be downloaded directly:
+            var xhr = new XMLHttpRequest();
+            xhr.responseType = 'blob';
+            xhr.onload = function (event) {
+                var blob = xhr.response;
+            };
+            xhr.open('GET', url);
+            xhr.send();
+
+            // Or inserted into an <img> element:
+            var img = document.getElementById('myimg');
+            img.src = url;
+        }).catch(function (error) {
+            // Handle any errors
+        });
+        return Ref;
+    } 
+
     render(){
-        const styles = StyleSheet.create({
-            container: {
-              alignItems: 'center'
-            },
-            title: {
-              fontSize: 30,
-              fontWeight: 'bold',
-              textAlign: 'center',
-              fontFamily: 'asd'
-            },
-           
-          });
         const {authReducer} = this.props
         
         return <View style={styles.container}>
-            <Text style = {{fontFamily:'asd',fontSize:30,fontWeight:'bold'}}>Your Profile</Text>
-            {authReducer.isAuth && <View>
-                <Avatar xlarge rounded source={{ uri: authReducer.data.photoURL }} onPress={() => console.log("Works!")} />
-                <Text style={{paddingTop:20,fontSize:15,textAlign:'center'}}>{authReducer.data.displayName}</Text>
-                <Text>{authReducer.data.Email}</Text>
-                <Text>Star:{authReducer.data.star}</Text>
-{/* //TODO แสดง เลเวล ค่าประสบการณ์ Objectที่เก็บข้อมูล = authReducer.data.levelQ */}
-                <Text style={{paddingBottom:70,textAlign:'center',paddingTop:60}}>Level:</Text>
-                <Button title="Achievement Earned" 
-                    buttonStyle={{
-                        backgroundColor: "rgba(00, 99,216, 1)",
-                        width: 120,
-                        height: 40,
-                        borderColor: "transparent",
-                        borderWidth: 0,
-                      }}  
-                />
-              </View>}
+            <Text>Your Achievement</Text>
+            <Image style={{width: 50, height: 50}}
+          source = {
+              {uri:'https://firebasestorage.googleapis.com/v0/b/project3f-4a950.appspot.com/o/achieve%2Ficon.png?alt=media&token=e95c5c83-7b5c-4db3-96f7-258b06b925a1'}
+          }
+        />
+            {
+            authReducer.isAuth && Object.values(this.state.achieve).map((obj,i) =>
+            <View key={i}>
+                <Text>Name: {obj.name}</Text>
+                <Text>Detail: {obj.detail}</Text>
+                <Text>Time: {obj.time}</Text>
+                <Text>star: {obj.star}</Text>
+                <Image style={{width: 50, height: 50}}
+          source = {
+              {
+                  uri: this.getUrlImage(obj.path) || 'https://firebasestorage.googleapis.com/v0/b/project3f-4a950.appspot.com/o/achieve%2Ficon.png?alt=media&token=e95c5c83-7b5c-4db3-96f7-258b06b925a1'
+              }
+          }
+        />
+                <Text> </Text>
+            </View>)}
           </View>;
     }
 }
