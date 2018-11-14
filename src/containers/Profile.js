ba6ce426d63ae148cf057e97f827f3f37e962922
CraZyNut <nutkungza05@hotmail.com>
2018-11-14 20:16:08 +0700

    Profile

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -2,6 +2,7 @@ import { View,Text,StyleSheet } from 'react-native';
 import React,{Component} from 'react';
 import { connect } from "react-redux";
 import { Button, Avatar } from 'react-native-elements';
+import ProgressBarAnimated from 'react-native-progress-bar-animated';
 
 
 class Profile extends Component{
@@ -32,12 +33,35 @@ class Profile extends Component{
         return <View style={styles.container}>
             <Text style = {{fontFamily:'asd',fontSize:30,fontWeight:'bold'}}>Your Profile</Text>
             {authReducer.isAuth && <View>
-                <Avatar xlarge rounded source={{ uri: authReducer.data.photoURL }} onPress={() => console.log("Works!")} />
+                <Avatar containerStyle = {{left:70}} large rounded source={{ uri: authReducer.data.photoURL }} onPress={() => console.log("Works!")} />
                 <Text style={{paddingTop:20,fontSize:15,textAlign:'center'}}>{authReducer.data.displayName}</Text>
                 <Text>{authReducer.data.Email}</Text>
                 <Text>Star:{authReducer.data.star}</Text>
 {/* //TODO แสดง เลเวล ค่าประสบการณ์ Objectที่เก็บข้อมูล = authReducer.data.levelQ */}
-                <Text style={{paddingBottom:70,textAlign:'center',paddingTop:60}}>Level:</Text>
+                <Text style={{textAlign:'center',paddingTop:10,paddingBottom:30}}>Level:</Text>
+                <View style = {{padding:5}}>
+                <ProgressBarAnimated width = {200}/>
+                </View>
+                <View style = {{padding:5}}>
+                <ProgressBarAnimated width = {200}/>
+                </View>
+                <View style = {{padding:5}}>
+                <ProgressBarAnimated width = {200}/>
+                </View>
+                <View style = {{paddingTop:10}}>
+                <Button title="Share Now!" 
+                    buttonStyle={{
+                        backgroundColor: "rgba(10, 10,100, 1)",
+                        width: 120,
+                        height: 30,
+                        borderColor: "transparent",
+                        borderWidth: 0,
+                        borderRadius:360,
+                        left:30
+                      }}  
+                />
+                </View>
+                <View style = {{paddingTop:10}}>
                 <Button title="Achievement Earned" 
                     buttonStyle={{
                         backgroundColor: "rgba(00, 99,216, 1)",
@@ -45,8 +69,10 @@ class Profile extends Component{
                         height: 40,
                         borderColor: "transparent",
                         borderWidth: 0,
+                        left:30
                       }}  
                 />
+                </View>
               </View>}
           </View>;
     }
